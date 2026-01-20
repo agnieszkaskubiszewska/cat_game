545e2daa8d41a71bf93e358b3da412cf63e7799b
Agnieszka Skubiszewska <agnieszkaskubiszewska@MacBook-Pro-Agnieszka.local>
2026-01-20 22:59:59 +0100

    Initial commit

---
+++ src/components/Game.jsx
@@ -0,0 +1,42 @@
+import { useRef, useEffect } from "react";
+
+export default function Game() {
+
+  const canvasRef = useRef(null);
+
+  const xRef= useRef(50);
+  const yRef = useRef(50); 
+
+const drawCat=(ctx,x,y)=>{
+  ctx.fillStyle='#1e1e1e'; 
+ctx.fillRect(0,0,320,180);
+ctx.fillStyle='#ffcc00'; 
+ctx.fillRect(x,y,32,32);
+}
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    drawCat(ctx, xRef.current, yRef.current);
+
+    const keyDownHandler=(e)=>{
+      if(e.key === 'ArrowRight'){
+        xRef.current += 16;
+      }
+      if(e.key === 'ArrowLeft'){
+        xRef.current -= 16;
+      }
+      if(e.key === 'ArrowUp'){
+        yRef.current -= 16;
+      }
+      if(e.key === 'ArrowDown'){
+        yRef.current += 16;
+      }
+      drawCat(ctx, xRef.current, yRef.current);
+    }
+window.addEventListener('keydown', keyDownHandler);
+return ()=>window.removeEventListener('keydown', keyDownHandler);
+  }, []);
+
+  return <canvas ref={canvasRef} width={320} height={180} className="game-canvas" />;
+}
